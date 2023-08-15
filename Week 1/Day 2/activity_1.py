1a2716812ca73d8106c430bee009c33c83373fb7
alexvei <balorianosx@gmail.com>
2023-08-15 11:29:21 +0100

    Even more day 2 stuff

---
+++ Week 1/Day 2/activity_1.py
@@ -0,0 +1,12 @@
+# Operators
+# # # # # #
+# We have arithmetic operators (+ - * ** / %)
+# We have assignment operators ( += -= *= /= =)
+
+print(1+1) # addition
+print(1-1) # subtraction
+print(1*1) # multiplication
+print(1/1) # division
+print(2**2) # expon/power
+print(3%2) # modulo, gives remainder
+
---
+++ Week 1/Day 2/activity_2.py
@@ -0,0 +1,6 @@
+# calculating the number of days from today to your birth date.
+# say that every month has 30 days. then today is the 7*30 + 16 th day.
+# now say the same logic, and find that my birthday is on the 5*30 + 2th day.
+# hence it will take:
+
+#print(365 - (7*30+16) + (5*30 + 2))
